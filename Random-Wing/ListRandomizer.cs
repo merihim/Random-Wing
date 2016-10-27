71702e8b8da0bc147e652464ea9800b410cd22b7
merihim <merihim@gmail.com>
2016-10-27 08:53:52 -0500

    Add files via upload

---
+++ Random-Wing/ListRandomizer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ListFactory;
+
+
+namespace Random_Wing
+{
+    class ListRandomizer
+    {
+        static void Main(string[] args)
+        {
+            ShipBuilder shipBuilder = new ShipBuilder();
+
+            shipBuilder.SetPoints();
+            shipBuilder.GenerateShipValues();
+            shipBuilder.CheckBudget();
+            shipBuilder.FindShipOfValue();
+            Console.ReadLine();
+        }
+    }
+}
